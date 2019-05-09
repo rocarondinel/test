5e3f154e7ad6a4aac3e19f25fd571405015cc6f1
Angel Roca <putazo@gmail.com>
2019-05-09 11:53:46 -0500

    chore: changing message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-project';
+  title = 'Angel se la come doblada y subtitulada y la mastica con ferocidad.... yum!';
 }
